c71d018ee5c3d34acce30e42b15082c090fb8464
pruzanov <pruzanov@oicr.on.ca>
2021-11-09 10:25:40 -0500

    improved processing of vcf.gz files

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 cd $1
 
-find .  -name "*\.vcf.gz" -exec unzip -q {} \; >/dev/null # unzip the results files
-
-# - .vcf files have no stochastic content except a header line with time information
+# - .vcf.gz files have no stochastic content except a header line with time information
 # - .tbi files are generically named, no stochastic content, may be md5sum-checked
 # - .fin files are generically named, no stochastic content, may be md5sum-checked
 
@@ -11,7 +9,7 @@ find .  -name "*\.vcf.gz" -exec unzip -q {} \; >/dev/null # unzip the results fi
 # - Check md5sums for all types of files, sort
 
 echo ".vcf files:"
-find . -name "*.vcf" | xargs grep -v GATKCommandLine | md5sum | sort
+for v in *.vcf.gz;do gunzip -c $v | grep -v GATKCommandLine | md5sum;done | sort -V
 
 echo ".tbi files:"
 find . -name "*.tbi" | xargs md5sum | sort
